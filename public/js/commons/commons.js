c3bb1686ac772f95cb0f39e86ae28a068b6c23b1
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2023-10-20 16:23:13 -0300

    css adjustments and login creation

--- public/js/commons/commons.js
+++ public/js/commons/commons.js
@@ -69,3 +69,15 @@ btnCadastrado.addEventListener('click', () => {
     cadastroContent.style.display = 'none';
     cadastradoContent.style.display = 'block';
 });
+
+const btnLogout = document.getElementById('btnLogout');
+
+if (btnLogout) {  // Adicionando uma verificação para garantir que o botão de logout exista na página atual
+    btnLogout.addEventListener('click', function() {
+        // Remove o token do local storage
+        localStorage.removeItem('token');
+
+        // Redireciona o usuário para a tela de login
+        window.location.href = 'http://127.0.0.1:5500/app/views/login/login.html'; // Lembre-se de atualizar com o caminho correto para sua tela de login
+    });
+}
--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const passwordInput = document.getElementById('user_password');
+    let passwordInput = document.getElementById('user_password');
+    const emailInput = document.getElementById('user_email');
     const passwordToggle = document.getElementById('password-toggle');
+    const loginButton = document.querySelector('.button-login button');
 
     passwordToggle.addEventListener('click', function() {
         if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
+            const clone = passwordInput.cloneNode(true);
+            clone.type = 'text';
+            clone.value = passwordInput.value;  // Manter o valor do campo de senha
+            passwordInput.parentNode.replaceChild(clone, passwordInput);
             passwordToggle.innerHTML = '<i class="bi bi-eye-slash-fill"></i>';
+            passwordInput = clone;  // Atualize a referência
         } else {
-            passwordInput.type = 'password';
+            const clone = passwordInput.cloneNode(true);
+            clone.type = 'password';
+            clone.value = passwordInput.value;  // Manter o valor do campo de senha
+            passwordInput.parentNode.replaceChild(clone, passwordInput);
             passwordToggle.innerHTML = '<i class="bi bi-eye-fill"></i>';
+            passwordInput = clone;  // Atualize a referência
         }
+    });
+
+    loginButton.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        if (!email || !password) {
+            alert('Por favor, preencha ambos os campos, email e senha.');
+            return;
+        }
+
+        fetch('http://localhost:3000/login', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, senha: password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                localStorage.setItem('token', data.token); // Armazenando o token no local storage
+
+                if (data.perfil === 'Padrão') {
+                    window.location.href = 'http://127.0.0.1:5500/app/views/user/userHome.html';
+                } else if (data.perfil === 'Gestor' || data.perfil === 'Administrador') {
+                    window.location.href = 'http://127.0.0.1:5500/app/views/administrator/administratorHome.html';
+                }
+            } else {
+                alert('Erro ao realizar login. Verifique suas credenciais.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao realizar login:', error);
+            alert('Erro ao realizar login. Por favor, tente novamente.');
+        });
     });
 });
