f66212be29ebe68826486897210bb2ee5a147040
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2023-10-26 16:22:21 -0300

    Feat: improves pagination and previous and next buttons

--- public/js/commons/pagination.js
+++ public/js/commons/pagination.js
@@ -3,76 +3,110 @@ const paginationList = document.getElementById('pagination-list');
 
 let currentPage = 0;
 const recordsPerPage = 8;
+const maxButtonsToShow = 5; // Número máximo de botões de página a serem exibidos de uma vez
 
 // Função para buscar os modelos no back-end usando o Axios
 async function fetchTemplates() {
-  try {
-    const response = await axios.get('http://localhost:3000/templates'); // Ajuste a URL para a rota correta do seu backend
-    const templates = response.data;
-    initPagination(templates);
-  } catch (error) {
-    console.error('Erro ao buscar os modelos:', error);
-  }
+    try {
+        const response = await axios.get('http://localhost:3000/templates'); 
+        const templates = response.data;
+        initPagination(templates);
+    } catch (error) {
+        console.error('Erro ao buscar os modelos:', error);
+    }
 }
 
 function displayTemplates(templates, page) {
-  const startIndex = page * recordsPerPage;
-  const endIndex = startIndex + recordsPerPage;
-  const displayedTemplates = templates.slice(startIndex, endIndex);
+    const startIndex = page * recordsPerPage;
+    const endIndex = startIndex + recordsPerPage;
+    const displayedTemplates = templates.slice(startIndex, endIndex);
 
-  templateList.innerHTML = '';
+    templateList.innerHTML = '';
 
-  displayedTemplates.forEach((template) => {
-    const downloadIconClass = template.status ? 'icon-active download-upload-icon' : 'icon-inactive download-upload-icon';
-    const uploadIconClass = template.status ? 'icon-active download-upload-icon' : 'icon-inactive download-upload-icon';
-    const templateRow = document.createElement('tr');
-    templateRow.innerHTML = `
-        <td>${template.nome_template}</td>
-        <td>${template.extensao_template}</td>
-        <td>${template.usuario.matricula}</td>
-        <td>${template.id_template}</td>
-        <td>${template.usuario.email}</td>
-        <td><i class="bi bi-download ${downloadIconClass}" title="Download"></i></td>
-        <td><i class="bi bi-upload ${uploadIconClass}" title="Upload"></i></td>
-        <td>${template.status ? 'Ativo' : 'Inativo'}</td>
-    `;
-    templateList.appendChild(templateRow);
-  });
+    displayedTemplates.forEach((template) => {
+        const downloadIconClass = template.status ? 'icon-active download-upload-icon' : 'icon-inactive download-upload-icon';
+        const uploadIconClass = template.status ? 'icon-active download-upload-icon' : 'icon-inactive download-upload-icon';
+        const templateRow = document.createElement('tr');
+        templateRow.innerHTML = `
+            <td>${template.nome_template}</td>
+            <td>${template.extensao_template}</td>
+            <td>${template.usuario.matricula}</td>
+            <td>${template.id_template}</td>
+            <td>${template.usuario.email}</td>
+            <td><i class="bi bi-download ${downloadIconClass}" title="Download"></i></td>
+            <td><i class="bi bi-upload ${uploadIconClass}" title="Upload"></i></td>
+            <td>${template.status ? 'Ativo' : 'Inativo'}</td>
+        `;
+        templateList.appendChild(templateRow);
+    });
 }
 
-
 function createPaginationButtons(templates) {
-  const totalPages = Math.ceil(templates.length / recordsPerPage);
-  paginationList.innerHTML = '';
+    const totalPages = Math.ceil(templates.length / recordsPerPage);
+    paginationList.innerHTML = '';
 
-  for (let i = 0; i < totalPages; i++) {
-    const button = document.createElement('button');
-    button.textContent = i + 1;
-    button.addEventListener('click', () => {
-      currentPage = i;
-      displayTemplates(templates, currentPage);
-      updatePaginationButtons(totalPages);
+    // Botão "Anterior"
+    const prevButton = document.createElement('button');
+    prevButton.textContent = 'Anterior';
+    prevButton.addEventListener('click', () => {
+        if (currentPage > 0) {
+            currentPage--;
+            displayTemplates(templates, currentPage);
+            updatePaginationButtons(totalPages);
+        }
     });
+    paginationList.appendChild(prevButton);
 
-    paginationList.appendChild(button);
-  }
+    const startPage = Math.max(0, currentPage - Math.floor(maxButtonsToShow / 2));
+    const endPage = Math.min(totalPages, startPage + maxButtonsToShow);
+
+    for (let i = startPage; i < endPage; i++) {
+        const button = document.createElement('button');
+        button.textContent = i + 1;
+        button.addEventListener('click', () => {
+            currentPage = i;
+            displayTemplates(templates, currentPage);
+            updatePaginationButtons(totalPages);
+        });
+        paginationList.appendChild(button);
+    }
+
+    // Botão "Próximo"
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Próximo';
+    nextButton.addEventListener('click', () => {
+        if (currentPage < totalPages - 1) {
+            currentPage++;
+            displayTemplates(templates, currentPage);
+            updatePaginationButtons(totalPages);
+        }
+    });
+    paginationList.appendChild(nextButton);
 }
 
 function updatePaginationButtons(totalPages) {
-  const buttons = paginationList.getElementsByTagName('button');
-  for (let i = 0; i < buttons.length; i++) {
-    if (i === currentPage) {
-      buttons[i].classList.add('active'); // Adicione a classe 'active' ao botão da página atual
-    } else {
-      buttons[i].classList.remove('active');
-    }
-  }
+    const buttons = Array.from(paginationList.getElementsByTagName('button'));
+    const prevButton = buttons[0];
+    const nextButton = buttons[buttons.length - 1];
+
+    // Atualiza botões de página
+    buttons.slice(1, buttons.length - 1).forEach((button, index) => {
+        if (index + 1 === currentPage) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+
+    // Ativa/desativa botões "Anterior" e "Próximo"
+    prevButton.disabled = currentPage === 0;
+    nextButton.disabled = currentPage === totalPages - 1;
 }
 
 function initPagination(templates) {
-  displayTemplates(templates, currentPage);
-  createPaginationButtons(templates);
-  updatePaginationButtons(Math.ceil(templates.length / recordsPerPage));
+    displayTemplates(templates, currentPage);
+    createPaginationButtons(templates);
+    updatePaginationButtons(Math.ceil(templates.length / recordsPerPage));
 }
 
-fetchTemplates(); // Chama a função para buscar os modelos quando a página é carregada
+fetchTemplates(); // Chama a função para buscar os modelos quando a página é carregada 
