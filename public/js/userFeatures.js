6f094af424dde5484182dc8816bae534d69bd4eb
Bruno Bilheri Monteiro <brunobilheri@hotmail.com>
2023-11-01 16:16:20 -0300

    Creating user search by registration

--- public/js/userFeatures.js
+++ public/js/userFeatures.js
@@ -44,3 +44,53 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Busca de usuário por matricula
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("#id-search-form");
+    const resultDiv = document.querySelector("#id-search-result");
+    const matriculaInput = form.querySelector("#id-matricula-input");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        const matricula = matriculaInput.value;
+
+        // Se a matrícula estiver vazia, esconde os resultados e retorna
+        if (!matricula) {
+            resultDiv.style.display = "none";
+            return;
+        }
+
+        // Buscar detalhes do usuário pela matrícula
+        fetch(`http://localhost:3000/usuario/${matricula}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.message === "Usuário não encontrado") {
+                alert("Usuário não encontrado. Por favor, tente com outra matrícula.");
+                resultDiv.style.display = "none";
+                return;
+            }
+
+            // Exibir os detalhes do usuário nos campos de formulário
+            document.querySelector("#id-display-matricula").value = data.matricula;
+            document.querySelector("#id-display-email").value = data.email;
+            document.querySelector("#id-display-nome").value = data.nome_usuario;
+            document.querySelector("#id-display-foto").value = data.foto_url;
+            document.querySelector("#id-display-perfil").value = data.perfil_acesso;
+
+            resultDiv.style.display = "flex"; // Mostra o resultado da busca
+        })
+        .catch(error => {
+            alert("Erro ao buscar usuário. Por favor, tente novamente.");
+        })
+        .finally(() => {
+            matriculaInput.value = ''; // Limpa o campo de matrícula após a pesquisa
+        });
+    });
+});
